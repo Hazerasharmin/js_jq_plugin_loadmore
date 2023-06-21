6646eca53c720ce6d88262d068d82c8d8bf0e6bc
Hazerasharmin <124484818+Hazerasharmin@users.noreply.github.com>
2023-06-21 20:10:59 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//js
+
+$('.list1 .loadMore').loadMoreResults();
+    $('.list2 .loadMore').loadMoreResults({
+      tag: {
+        name: 'li',
+        'class': 'thumbnail'
+      },
+      displayedItems: 5,
+      showItems: 2
+    });
+
+
+//jq
+
+
+
+
+
